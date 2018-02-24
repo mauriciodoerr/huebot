f93b3e80b7703415da4586067d46d8bc481c78b3
mdoerr <mauricio.doerr@ymail.com>
2018-02-24 16:23:36 -0300

    Variaveis de ambiente adicionadas

--- app.js
+++ app.js
@@ -1,47 +1,64 @@
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
 var restify = require('restify');
 var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
 
-bot.dialog('/', [
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+                knowledgeBaseId: process.env.QnAKnowledgebaseId, 
+    subscriptionKey: process.env.QnASubscriptionKey});
 
-    function (session) {
-        builder.Prompts.text(session, "Hello, and welcome to QnA Factbot!What's your name?");
-    },
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+                defaultMessage: 'No match! Try changing the query terms!',
+                qnaThreshold: 0.3}
+);
 
-    function (session, results) {
+bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
 
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", how many years have you been writing code?");
-    },
-
-    function (session, results) {
-
-        session.userData.yearsCoding = results.response;
-        builder.Prompts.choice(session, "What language do you love the most?", ["C#", "JavaScript", "TypeScript", "Visual FoxPro"]);
-    },
-
-    function (session, results) {
-
-        session.userData.language = results.response.entity;
-
-        session.send("Okay, " + session.userData.name + ", I think I've got it:" +
-                    " You've been writing code for " + session.userData.yearsCoding + " years," +
-                    " and prefer to use " + session.userData.language + ".");
-    }]);
+bot.dialog('/', //basicQnAMakerDialog);
+[
+    function (session){
+        var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+        var qnaSubscriptionKey = process.env.QnASubscriptionKey;
+        
+        // QnA Subscription Key and KnowledgeBase Id null verification
+        if((qnaSubscriptionKey == null || qnaSubscriptionKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
+            session.send('Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.');
+        else
+            session.replaceDialog('basicQnAMakerDialog');
+    }
+]);
